482f713471fa2aa0d6a91102add62dd05fbe81e8
jueunnnn <jueun@g.skku.edu>
2025-04-12 08:09:28 +0900

    Initial commit

---
+++ screen.js
@@ -0,0 +1,5 @@
+// 버튼 클릭 이벤트 설정
+document.getElementById("clickButton").addEventListener("click", function () {
+  // 결과 텍스트 변경
+  document.getElementById("result").textContent = "버튼이 클릭되었습니다!";
+});
